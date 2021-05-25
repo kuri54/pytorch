4fbdc05e5832742420bde75735710fb4934fb979
kuri54 <kurikuri54@gmail.com>
2021-05-25 10:07:50 +0900

    add anomaly-detection

---
+++ anomaly-detection/autoencorder-mnist.py
@@ -0,0 +1,13 @@
+# %%
+import os
+import numpy as np
+import torch
+from torch import nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader,Dataset
+from torchvision import transforms
+from torchvision.datasets import MNIST
+import pylab
+import matplotlib.pyplot as plt
+
+# %%
